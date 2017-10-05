42013d90f64d721f4916940f7bd9a2fad6ea5266
Teodor Halvadzhiev <thalvadzhiev@gmail.com>
2017-10-06 00:24:21 +0300

    using hash for descriptions

--- descriptions_getter/directions_getter.py
+++ descriptions_getter/directions_getter.py
@@ -6,7 +6,8 @@ import logging
 from git_client import *
 from push_notification import *
 import multiprocessing
-
+import hashlib
+import os
 
 linesBaseURL = "https://api.sofiatransport.com/v3/lines/{0}"
 linesStopsURL = "https://api.sofiatransport.com/v3/lines/{0}/{1}"
@@ -17,7 +18,8 @@ stop_identity = "{0},{1},{2}={3}"
 stop_ids = set()
 counter = [0]
 line_types = [0, 1, 2]
-
+hash_file_name = "hash.txt"
+hash_file_name_temp = "new_hash.txt"
 logging.basicConfig(format='%(asctime)s %(message)s', filename=log_file_name, level=logging.INFO)
 
 with open('config.json', 'r') as f:
@@ -42,6 +44,7 @@ def get_all_lines():
         for line in d:
             job = DownloadJob(line)
             pool.put(job)
+        print("Line type "+str(line_type) )
     pool.shutdown()
     pool.wait()
     logging.info("Finished getting stops descriptions.")
@@ -51,15 +54,17 @@ def read_line_stops(line):
     url =linesStopsURL.format(line["type"], line["id"])
     resp = requests.get(url, headers=headers)
     d = resp.json()
-    print(d)
-    routes = d['routes']
-    for route in routes:
-        stops = route['stops']
-        direction = direction_template.format(stops[0]["name"], stops[len(stops) - 1]["name"])
-        for stop in stops:
-            stop_id = stop_identity.format(line["type"], line["name"], stop["code"], direction)
-            stop_ids.add(stop_id)
-            counter[0] += 1
+    if 'routes' not in d:
+        print(d)
+    else:
+        routes = d['routes']
+        for route in routes:
+            stops = route['stops']
+            direction = direction_template.format(stops[0]["name"], stops[len(stops) - 1]["name"])
+            for stop in stops:
+                stop_id = stop_identity.format(line["type"], line["name"], stop["code"], direction)
+                stop_ids.add(stop_id)
+                counter[0] += 1
 
 def generate_stops_desc(stops):
     return ";".join(stops)
@@ -82,18 +87,49 @@ def write_to_file(stops_desc):
 def upload_coordinates():
     logging.info("Uploading changes...")
     add_file('descriptions_getter/' + desc_file_name)
+    add_file('descriptions_getter/' + hash_file_name)
     commit("Descriptions update")
     push()
     logging.info("Done uploading!")
 
+def calculate_hash(stops_descs):
+    sha256 = hashlib.sha256()
+    sha256.update(stops_descs.encode())
+    new_hash = sha256.hexdigest()
+    return new_hash
+
+def write_hash_to_file(hash_to_write):
+    with open(hash_file_name_temp, "w") as f:
+        f.write(hash_to_write)
+
+def stops_descs_have_changed_hash():
+    if not os.path.isfile(hash_file_name):
+        os.rename(hash_file_name_temp, hash_file_name)
+        return True
+
+    with open(hash_file_name_temp, "r") as new, open(hash_file_name, "r") as old:
+        new_read = new.read()
+        old_read = old.read()
+        if old_read != new_read:
+            os.remove(hash_file_name)
+            os.rename(hash_file_name_temp, hash_file_name)
+            return True
+        else:
+            os.remove(hash_file_name_temp)
+            return False
+
 def main():
     start = time.time()
+    print("yay")
     get_all_lines()
+    print("finished")
     stop_ids_l = list(stop_ids)
     stop_ids_l.sort()
 
     stops_desc = generate_stops_desc(stop_ids_l)
-    if stops_descs_have_changed(stops_desc):
+    hash_new = calculate_hash(stops_desc)
+    write_hash_to_file(hash_new)
+    if stops_descs_have_changed_hash():
         logging.info("Stops descriptions have changed!")
         write_to_file(stops_desc)
         upload_coordinates()
