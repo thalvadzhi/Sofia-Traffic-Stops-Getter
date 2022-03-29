700036f4f381959d485b6132ec4c55a35bf99c2f
Teodor <thalvadzhiev@gmail.com>
2022-03-29 16:23:15 +0300

    Switch to new api for stops getting

--- get_everything.py
+++ get_everything.py
@@ -1,4 +1,4 @@
-from stops_getter.stops_getter import run_stop_getter
+from stops_getter.stops_getter_new_api import run_stop_getter
 from polyline_getter.polyline_getter import run_polyline_getter
 from descriptions_getter.directions_getter import run_directions_getter
 
