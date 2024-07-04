ab9d8c5a6eca562818ae1597d87bc066b0397c45
Teodor Halvadzhiev <teodor.halvadzhiev@0la.vobileinc.com>
2024-07-05 00:28:58 +0300

    Switch get_everything script to new scripts

--- get_everything.py
+++ get_everything.py
@@ -1,6 +1,6 @@
-from stops_getter.stops_getter_new_api import run_stop_getter
-from polyline_getter.polyline_getter import run_polyline_getter
-from descriptions_getter.directions_getter import run_directions_getter
+from stops_getter.stops_getter_newer_api import run_stop_getter
+from polyline_getter.polyline_getter_new import run_polyline_getter
+from descriptions_getter.directions_getter_new import run_directions_getter
 
 if __name__ == "__main__":
     run_stop_getter(should_upload=True)
