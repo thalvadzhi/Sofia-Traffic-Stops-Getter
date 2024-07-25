4cad30d696c4c49857579b9582eeed1b2bd5ad87
Teodor Halvadzhiev <teodor.halvadzhiev@0la.vobileinc.com>
2024-07-26 02:25:56 +0300

    Set network mode to bridge

--- get_everything.py
+++ get_everything.py
@@ -3,6 +3,6 @@ from polyline_getter.polyline_getter_new import run_polyline_getter
 from descriptions_getter.directions_getter_new import run_directions_getter
 
 if __name__ == "__main__":
-    run_stop_getter(should_upload=True)
-    run_polyline_getter(should_upload=True)
-    run_directions_getter(should_upload=True)
+    run_stop_getter(should_upload=False)
+    run_polyline_getter(should_upload=False)
+    run_directions_getter(should_upload=False)
