1da7086e21ab199c030b79678e1e481a2810edf6
Teodor Halvadzhiev <teodor.halvadzhiev@0la.vobileinc.com>
2024-07-28 13:11:28 +0300

    Add ssh key from entry script

--- docker/entry_script.sh
+++ docker/entry_script.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 printenv  >> /etc/environment
+cp /run/secrets/ssh_key_stops_getter /root/.ssh/id_rsa
+
 git remote set-url origin git@github.com:thalvadzhi/Sofia-Traffic-Stops-Getter.git
 
 cron -f
