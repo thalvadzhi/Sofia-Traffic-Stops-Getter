89901f170568a5f9a990143536094a1d9eeca8a0
Teodor Halvadzhiev <teodor.halvadzhiev@0la.vobileinc.com>
2024-07-28 12:43:09 +0300

    Copy ssh key to .ssh/config

--- docker/entry_script.sh
+++ docker/entry_script.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/bash
 printenv  >> /etc/environment
 git remote set-url origin git@github.com:thalvadzhi/Sofia-Traffic-Stops-Getter.git
-sudo eval "$(ssh-agent -s)"
-
-sudo ssh-add $SSH_KEY
 
 cron -f
