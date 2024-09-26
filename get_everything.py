48c1d93a90f10f7aa20451284dd7b1152c7b41df
Теодор Халваджиев <thalvadzhiev@gmail.com>
2024-09-26 12:41:17 +0300

    Update get_everything.py

--- get_everything.py
+++ get_everything.py
@@ -2,7 +2,8 @@ from stops_getter.stops_getter_newer_api import run_stop_getter
 from polyline_getter.polyline_getter_new import run_polyline_getter
 from descriptions_getter.directions_getter_new import run_directions_getter
 
-if __name__ == "__main__":
-    run_stop_getter(should_upload=True)
-    run_polyline_getter(should_upload=True)
-    run_directions_getter(should_upload=True)
+def main():
+    if __name__ == "__main__":
+        run_stop_getter(should_upload=True)
+        run_polyline_getter(should_upload=True)
+        run_directions_getter(should_upload=True)
