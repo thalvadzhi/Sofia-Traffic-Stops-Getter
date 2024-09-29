2f601c203f3b30d03ee99e28b4566e6c48d6d752
Teodor Halvadzhiev <teodor.halvadzhiev@0la.vobileinc.com>
2024-09-30 01:11:54 +0300

    Update main to be able to use in lambda

--- get_everything.py
+++ get_everything.py
@@ -2,7 +2,7 @@ from stops_getter.stops_getter_newer_api import run_stop_getter
 from polyline_getter.polyline_getter_new import run_polyline_getter
 from descriptions_getter.directions_getter_new import run_directions_getter
 
-def main():
+def main(event, context):
     if __name__ == "__main__":
         run_stop_getter(should_upload=True)
         run_polyline_getter(should_upload=True)
