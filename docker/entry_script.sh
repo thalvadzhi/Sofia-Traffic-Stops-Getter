f59b0c2c885c27ad320344b2711ff3ebe2126f07
Teodor Halvadzhiev <teodor.halvadzhiev@0la.vobileinc.com>
2024-10-04 02:02:51 +0300

    Decode base64 key

--- docker/entry_script.sh
+++ docker/entry_script.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 printenv  >> /etc/environment
 mkdir -p /root/.ssh
-#cp /run/secrets/ssh_key_stops_getter /root/.ssh/id_rsa
-echo $SSH_KEY > /root/.ssh/id_rsa
+#cp /run/secrets/ssh_key_stops_getter /root/.ssh/id_rsa]
+echo "$SSH_KEY" | base64 -d > /root/.ssh/id_rsa
+
+#echo $SSH_KEY > /root/.ssh/id_rsa
 chmod 400 /root/.ssh/id_rsa
 
 
