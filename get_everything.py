664ac6e47b2560f526527855a8491145743d1552
Teodor Halvadzhiev <teodor.halvadzhiev@0la.vobileinc.com>
2024-10-04 00:54:08 +0300

    Update the get everything script

--- get_everything.py
+++ get_everything.py
@@ -2,7 +2,12 @@ from stops_getter.stops_getter_newer_api import run_stop_getter
 from polyline_getter.polyline_getter_new import run_polyline_getter
 from descriptions_getter.directions_getter_new import run_directions_getter
 
-def main(event, context):
-    run_stop_getter(should_upload=True)
-    run_polyline_getter(should_upload=True)
-    run_directions_getter(should_upload=True)
+def main():
+    print("Running stop getter")
+    run_stop_getter(should_upload=False)
+    print("Running polyline getter")
+    run_polyline_getter(should_upload=False)
+    print("Running directions getter")
+    run_directions_getter(should_upload=False)
+
+main()
