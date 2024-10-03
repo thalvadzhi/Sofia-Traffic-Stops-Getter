e7ad1be23bb1fe5bd4af5df2117c29870f50695c
Teodor Halvadzhiev <teodor.halvadzhiev@0la.vobileinc.com>
2024-10-04 01:41:23 +0300

    Use ssh_key env var

--- docker/entry_script.sh
+++ docker/entry_script.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 printenv  >> /etc/environment
 mkdir -p /root/.ssh
-cp /run/secrets/ssh_key_stops_getter /root/.ssh/id_rsa
+#cp /run/secrets/ssh_key_stops_getter /root/.ssh/id_rsa
+echo $SSH_KEY > /root/.ssh/id_rsa
 
 git remote set-url origin git@github.com:thalvadzhi/Sofia-Traffic-Stops-Getter.git
 
