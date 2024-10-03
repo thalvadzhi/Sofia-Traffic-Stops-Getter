0db811ab9679549707f904b94223e41a241feae7
Teodor Halvadzhiev <teodor.halvadzhiev@0la.vobileinc.com>
2024-10-04 01:47:54 +0300

    Change permission for private key

--- docker/entry_script.sh
+++ docker/entry_script.sh
@@ -3,6 +3,8 @@ printenv  >> /etc/environment
 mkdir -p /root/.ssh
 #cp /run/secrets/ssh_key_stops_getter /root/.ssh/id_rsa
 echo $SSH_KEY > /root/.ssh/id_rsa
+chmod 400 /root/.ssh/id_rsa
+
 
 git remote set-url origin git@github.com:thalvadzhi/Sofia-Traffic-Stops-Getter.git
 
